c7eb936a287a979509d1176a3f2ba4e074b32267
xxshinigamix <anderfigueiredo39@gmail.com>
2025-02-07 14:32:33 -0400

    Update script.js

--- public/script.js
+++ public/script.js
@@ -101,8 +101,8 @@ async function fazerAposta() {
         qrcodeContainer.innerHTML = '';
         new QRCode(qrcodeContainer, {
             text: dataPix.qr_code,
-            width: 100,
-            height: 100,
+            width: 50,
+            height: 50,
         });
 
         // Mostrar o "Copia e Cola"
